6291bd009cd054915ab5828df2af9fd2f53a5953
Jose Fiascaro <53021912+jfiascaro@users.noreply.github.com>
2020-02-10 20:15:01 -0600

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+import { APP_ROUTES } from './app.route';
+
+
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { GraphsComponent } from './pages/graphs/graphs.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    ProgressComponent,
+    GraphsComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/graphs/graphs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graphs',
+  templateUrl: './graphs.component.html',
+  styles: []
+})
+export class GraphsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
